3006d516deb667f06a0c2919fa730a7fed772494
taehan266-pixel <taehan266@gmail.com>
2025-12-16 21:57:57 +0900

    상품컴포넌트

---
+++ src/component/Productset.tsx
@@ -0,0 +1,36 @@
+
+import mainData from '../json/data.json';
+import type { MainProductSection } from '../types/banner';
+
+
+export default function Productset() {
+  const products = mainData.mainProduct as MainProductSection;
+  return (
+
+    <section className='max-w-1550 mx-auto py-[100px]'>
+         <h2 className='text-title font-600 mb-[30px]'> 
+           { products.section_title }
+         </h2>
+           <ul className='flex gap-[16px] '>
+            {
+              products.products.map((v, i)=>{
+                return(
+                  <li key={i} className='flex-1'>
+                    <img src={v.이미지}></img>
+                    <div className='flex flex-col gap-[8px]'>
+                      <p>{v.brand}</p>
+                      <p className='mb-2'>{v.name}</p>
+                      <p className='flex gap-4 items-end'>
+                         <span className='font-500 text-main'>{Math.round((1 - v.price / v.original_price) * 100)}%</span>
+                         <span>{v.price.toLocaleString()}원</span>
+                         <span>{v.original_price.toLocaleString()}원</span>
+                      </p>
+                    </div>
+                  </li>
+                )
+              })
+            }
+           </ul>
+       </section>
+  )
+}
